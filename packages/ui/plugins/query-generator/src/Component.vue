d3836030865a3476d56f00cbdcfc48ebe4b997eb
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-05 12:59:14 -0500

    basic setup with codemirror

--- packages/ui/plugins/query-generator/src/Component.vue
+++ packages/ui/plugins/query-generator/src/Component.vue
@@ -1,16 +1,15 @@
 <template>
-  <div ref="root" />
+  <div ref="root" :style="{ height: 100 }" />
 </template>
 
 <script lang="ts">
-import "codemirror/lib/codemirror.css"
-import CodeMirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "./assets/monokai-sublime.css";
+import CodeMirror from "codemirror/lib/codemirror.js";
+import "codemirror-graphql/mode";
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  // props: {
-  //   schema: GraphQLIntrospectionResult,
-  // },
   setup() {
     return {
       root: ref(null),
@@ -19,15 +18,44 @@ export default defineComponent({
   mounted() {
     CodeMirror(
       (elt) => {
-        this.root.parentNode.replaceChild(elt, this.root);
+        this.root.innerHTML = "";
+        this.root.appendChild(elt);
       },
       {
         lineNumbers: true,
-        value: "function myScript(){return 150;}\n",
-        mode: "javascript",
+        value: `{
+  test(variables: {input: "test"}) {
+    query
+  }
+}`,
+        theme: "monokai-sublime",
+        mode: "graphql",
         disabled: true,
+        readOnly: true,
+        lineNumbers: true,
+        indentUnit: 2,
+        cursorBlinkRate: 0,
+        cursorHeight: 0,
+        smartIndent: true,
+        dragDrop: false,
+        spellcheck: false,
       }
     );
   },
 });
 </script>
+
+<style>
+@import url(https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/fira_code.css);
+
+code {
+  font-family: "Fira Code", monospace;
+}
+
+.CodeMirror {
+  height: auto;
+  font-family: "Fira Code";
+  font-size: 1.4em;
+  line-height: 1.5em;
+}
+</style>
