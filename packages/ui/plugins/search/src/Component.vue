5076f8d929255a05a07418891e9c6a9a473d7f45
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-07 21:18:39 -0500

    poc with flexsearch

--- packages/ui/plugins/search/src/Component.vue
+++ packages/ui/plugins/search/src/Component.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script lang="ts">
+import { Document } from 'flexsearch'
 import { defineComponent, PropType } from 'vue'
 import { GraphQLSchema } from 'graphql'
 
@@ -12,6 +13,32 @@ export default defineComponent({
       type: Object as PropType<GraphQLSchema>,
       required: true,
     },
+  },
+  data() {
+    return {
+      index: new Document({
+        tokenize: 'full',
+        document: {
+          id: 'main',
+          field: ['label', 'munchkin'],
+        },
+      }),
+    }
+  },
+  mounted() {
+    this.index.add({
+      id: 'test',
+      label: 'potato',
+    })
+    this.index.add({
+      id: 'test-2',
+      munchkin: 'wtf pot',
+    })
+    this.index.add({
+      id: 'test-3',
+      munchkin: 'unrelated',
+    })
+    console.log(this.index.search('po'))
   },
 })
 </script>
