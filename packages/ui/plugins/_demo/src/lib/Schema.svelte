34df1fb26d8d6548817fcbf7b26c9eba88a33196
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-11 17:28:02 -0500

    add query generator 2

--- packages/ui/plugins/_demo/src/lib/Schema.svelte
+++ packages/ui/plugins/_demo/src/lib/Schema.svelte
@@ -1,4 +1,9 @@
-<script>
+<script lang="ts">
+  import schemaJson from '../assets/_schema.json'
+  import { IntrospectionQuery, buildClientSchema } from 'graphql'
+  import { readable } from 'svelte/store'
 
-const store = readable();
+  export const schema = readable(
+    buildClientSchema(schemaJson as unknown as IntrospectionQuery),
+  )
 </script>
---
+++ packages/ui/plugins/query-generator-2/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'query-generator',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
