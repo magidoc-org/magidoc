d87ec08e78cdd15c3568492e53b7cce782d4e913
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-12 15:04:03 -0500

    add logo

--- packages/ui/starters/single-page/src/routes/__layout.svelte
+++ packages/ui/starters/single-page/src/routes/__layout.svelte
@@ -21,17 +21,9 @@
 
 <main>
   <Header company="Qohash" platformName="Qostodian" bind:isSideNavOpen>
-    <HeaderNav>
-      <HeaderNavItem href="/" text="Link 1" />
-      <HeaderNavItem href="/" text="Link 2" />
-      <HeaderNavItem href="/" text="Link 3" />
-      <HeaderNavMenu text="Menu">
-        <HeaderNavItem href="/" text="Link 1" />
-        <HeaderNavItem href="/" text="Link 2" />
-        <HeaderNavItem href="/" text="Link 3" />
-      </HeaderNavMenu>
-      <HeaderNavItem href="/" text="Link 4" />
-    </HeaderNav>
+    <slot name="platform">
+		<
+	</slot>
   </Header>
 
   <SideNav isOpen={isSideNavOpen}>
@@ -53,40 +45,3 @@
     <slot />
   </Content>
 </main>
-
-<footer>
-  <p>
-    visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-  </p>
-</footer>
-
-<style>
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 1024px;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 40px 0;
-    }
-  }
-</style>
--- packages/ui/starters/single-page/src/routes/index.svelte
+++ packages/ui/starters/single-page/src/routes/index.svelte
@@ -18,32 +18,3 @@
     try editing <strong>src/routes/index.svelte</strong>
   </h2>
 </section>
-
-<style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-  }
-
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
-</style>
