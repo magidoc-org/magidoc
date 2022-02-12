8e0a7c13a56bcd2dce8bc53cb2986ef1f2e55a90
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-11 21:40:27 -0500

    add starter with carbon

--- packages/ui/plugins/_demo/src/main.ts
+++ packages/ui/plugins/_demo/src/main.ts
@@ -1,7 +1,13 @@
 import App from './App.svelte'
+import 'carbon-components-svelte/css/g90.css'
+
+const element = document.getElementById('app')
+if (!element) {
+  throw new Error('Expected element to exist with ID `app`')
+}
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: element,
 })
 
 export default app
