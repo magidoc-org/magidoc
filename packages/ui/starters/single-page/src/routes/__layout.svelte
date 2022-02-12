be051d3cc10acc37c846ead5cdd587417391895e
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-11 21:42:36 -0500

    add missing code

---
+++ packages/ui/starters/single-page/src/routes/__layout.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  import {
+    Content,
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    SideNav,
+    SideNavDivider,
+    SideNavItems,
+    SideNavLink,
+    SideNavMenu,
+    SideNavMenuItem,
+    SkipToContent,
+  } from 'carbon-components-svelte'
+
+  import 'carbon-components-svelte/css/g10.css'
+
+  let isSideNavOpen = true
+</script>
+
+<main>
+  <Header company="Qohash" platformName="Qostodian" bind:isSideNavOpen>
+    <HeaderNav>
+      <HeaderNavItem href="/" text="Link 1" />
+      <HeaderNavItem href="/" text="Link 2" />
+      <HeaderNavItem href="/" text="Link 3" />
+      <HeaderNavMenu text="Menu">
+        <HeaderNavItem href="/" text="Link 1" />
+        <HeaderNavItem href="/" text="Link 2" />
+        <HeaderNavItem href="/" text="Link 3" />
+      </HeaderNavMenu>
+      <HeaderNavItem href="/" text="Link 4" />
+    </HeaderNav>
+  </Header>
+
+  <SideNav isOpen={isSideNavOpen}>
+    <SideNavItems>
+      <SideNavLink text="Link 1" />
+      <SideNavLink text="Link 2" />
+      <SideNavLink text="Link 3" />
+      <SideNavMenu text="Menu">
+        <SideNavMenuItem href="/" text="Link 1" />
+        <SideNavMenuItem href="/" text="Link 2" />
+        <SideNavMenuItem href="/" text="Link 3" />
+      </SideNavMenu>
+      <SideNavDivider />
+      <SideNavLink text="Link 4" />
+    </SideNavItems>
+  </SideNav>
+
+  <Content>
+    <slot />
+  </Content>
+</main>
+
+<footer>
+  <p>
+    visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+  </p>
+</footer>
+
+<style>
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+  }
+
+  footer a {
+    font-weight: bold;
+  }
+
+  @media (min-width: 480px) {
+    footer {
+      padding: 40px 0;
+    }
+  }
+</style>
