02a156be1aa365c5bdfe5e373c94270e9ace1ecc
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-19 15:32:52 -0500

    add missing config files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['svelte3', '@typescript-eslint'],
+  parserOptions: {
+    project: ['packages/**/tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  settings: {
+    'svelte3/typescript': true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+}
