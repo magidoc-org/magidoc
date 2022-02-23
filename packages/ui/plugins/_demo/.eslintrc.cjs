130ef15472fa928e356f89a999fe54236a62e781
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-02-23 18:48:45 -0500

    add all eslint files

---
+++ packages/ui/plugins/_demo/.eslintrc.cjs
@@ -0,0 +1,9 @@
+const base = require('../../../../.eslintrc.js')
+
+module.exports = {
+  ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    project: [__dirname + '/tsconfig.json'],
+  },
+}
