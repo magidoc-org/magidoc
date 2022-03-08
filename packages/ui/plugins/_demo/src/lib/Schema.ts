ddb05c83b9d878c142cfe8b0109eaa1c69e488ae
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-03-07 19:46:22 -0500

    fix

--- packages/ui/plugins/_demo/src/lib/Schema.ts
+++
@@ -1,7 +0,0 @@
-import schemaJson from '../assets/_schema.json'
-import { IntrospectionQuery, buildClientSchema } from 'graphql'
-import { readable } from 'svelte/store'
-
-export const schema = readable(
-  buildClientSchema(schemaJson as unknown as IntrospectionQuery),
-)
--- packages/ui/starters/multi-page/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-const base = require('../../../../.eslintrc.js')
-
-module.exports = {
-  ...base,
-  parserOptions: {
-    ...base.parserOptions,
-    project: [__dirname + '/tsconfig.json'],
-  },
-}
