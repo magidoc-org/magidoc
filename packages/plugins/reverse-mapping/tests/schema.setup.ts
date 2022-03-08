79c6a2d7d815f0c6ac24eb6fc8f3ac35f0809ff6
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-03-07 20:01:45 -0500

    add reverse mapping project

---
+++ packages/plugins/reverse-mapping/tests/schema.setup.ts
@@ -0,0 +1,31 @@
+import {
+  buildClientSchema,
+  GraphQLField,
+  GraphQLObjectType,
+  GraphQLSchema,
+  IntrospectionQuery,
+} from 'graphql'
+import { Maybe } from 'graphql/jsutils/Maybe'
+import schema from './_schema.json'
+
+declare global {
+  function getTestSchema(): GraphQLSchema
+
+  function getMandatoryField(
+    type: Maybe<GraphQLObjectType>,
+    name: string,
+  ): GraphQLField<unknown, unknown, unknown>
+}
+
+global.getTestSchema = () =>
+  buildClientSchema(schema as unknown as IntrospectionQuery)
+
+global.getMandatoryField = (type: Maybe<GraphQLObjectType>, name: string) => {
+  const result = type?.getFields()[name]
+
+  if (!result) {
+    fail(`field ${name} should not be null... did you modify the test schema?`)
+  }
+
+  return result
+}
