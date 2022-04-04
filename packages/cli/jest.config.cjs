6b783fa9c5f35292e2ab947e77610c79ec602ff5
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-04-03 20:09:18 -0400

    add starter cli

---
+++ packages/cli/jest.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/tests/**/*.spec.ts'],
+  collectCoverage: true,
+  setupFilesAfterEnv: [
+    "jest-extended/all",
+    '<rootDir>/tests/global.setup.ts',
+    '<rootDir>/tests/schema.setup.ts',
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+}
