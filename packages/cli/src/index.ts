08617c40472bce65d937536ad60a949425d0bac3
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-04-09 14:35:02 -0400

    add index for cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -17,7 +17,7 @@ program
   .description(
     "Generates a full static website using a template. Using this option doesn't give you the ability to customize the output website. If you wish to customize the website, use the init command to generate a fresh project.",
   )
-  
+
 program
   .command('init')
   .description(
