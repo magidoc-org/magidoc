8a26e61ba13fd6408e30e40e836eec8bf141c24d
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-04-09 14:34:29 -0400

    add

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,28 @@
-export const x = ''
+import { Command } from 'commander'
+import { readFileSync } from 'fs'
+
+const packageJson = JSON.parse(readFileSync('../package.json').toString()) as {
+  version: string
+}
+
+const program = new Command()
+  .name('Magidoc')
+  .description(
+    'Magidoc CLI helps you to get started building GraphQL documentation websites.\n',
+  )
+  .version(packageJson.version || 'unknown')
+
+program
+  .command('generate')
+  .description(
+    "Generates a full static website using a template. Using this option doesn't give you the ability to customize the output website. If you wish to customize the website, use the init command to generate a fresh project.",
+  )
+  
+program
+  .command('init')
+  .description(
+    'Initializes a fresh documentation project using a template. You will then have the possibility to modify that template how you wish.\n',
+  )
+
+const args = program.parse()
+console.log(args)
