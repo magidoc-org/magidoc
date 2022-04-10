f660aa12dc4ef9c8711e04b5aa583f084085bcfd
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-04-10 17:43:29 -0400

    add zip starters

---
+++ packages/starters/zipStarters.cjs
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const path = require('path')
+const archiver = require('archiver')
+const { start } = require('repl')
+
+const basePath = __dirname
+
+const excludedPatterns = ['.svelte-kit/**', 'build/**', 'node_modules/**']
+
+function listStarterDirectories() {
+  const files = fs.readdirSync(basePath)
+  return files
+    .map((file) => path.join(basePath, file))
+    .filter((file) => {
+      const stat = fs.statSync(file)
+      return stat.isDirectory()
+    })
+}
+
+async function zipStarter(starterDirectory) {
+  const outputPath = path.join(
+      path.dirname(starterDirectory),
+      `starter-${path.basename(starterDirectory)}.zip`
+  )
+  fs.rmSync(outputPath, { force: true })
+
+  const output = fs.createWriteStream(outputPath)
+  const archive = archiver('zip', {
+    zlib: { level: 9 }, // 1 = best speed, 9 = best compression
+  })
+
+  output.on('close', function () {
+    console.log(`Wrote ${archive.pointer()} total bytes to ${outputPath}`)
+  })
+
+  // good practice to catch warnings (ie stat failures and other non-blocking errors)
+  archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') {
+      console.warn(err)
+    } else {
+      throw err
+    }
+  })
+
+  // good practice to catch this error explicitly
+  archive.on('error', function (err) {
+    throw err
+  })
+
+  archive.pipe(output)
+  archive.glob(`**/*`, {
+    dot: true,
+    cwd: starterDirectory,
+    ignore: excludedPatterns,
+  })
+
+  await archive.finalize()
+}
+
+const starters = listStarterDirectories()
+starters.forEach((path) => {
+  zipStarter(path)
+})
