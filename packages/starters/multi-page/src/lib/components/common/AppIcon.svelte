88f2beef4e9cbc84064cc046209b00e6e3f4ab35
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-04-19 21:29:25 -0400

    upgrade deps and change app logo variable

--- packages/starters/multi-page/src/lib/components/common/AppIcon.svelte
+++ packages/starters/multi-page/src/lib/components/common/AppIcon.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { common } from '@magidoc/plugin-starter-variables'
 
-  const logoPath = common.APP_LOGO_PATH.vite.getOrDefault(
+  const logoPath = common.APP_LOGO.vite.getOrDefault(
     import.meta.env,
     'https://github.com/magidoc-org/magidoc/blob/main/logo/logo_horizontal.png?raw=true',
   )
