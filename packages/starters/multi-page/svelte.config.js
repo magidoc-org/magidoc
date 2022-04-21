97eb499a1a81b5f728cea82cb52f87a8262aeda8
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-04-21 18:16:15 -0400

    update deps

--- packages/starters/multi-page/svelte.config.js
+++ packages/starters/multi-page/svelte.config.js
@@ -2,6 +2,7 @@ import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
 import { optimizeImports } from 'carbon-preprocess-svelte'
 import fetchGraphQLSchema from '@magidoc/rollup-plugin-fetch-gql-schema'
+import { magidoc } from '@magidoc/plugin-starter-variables'
 
 /**
  * @type {import('@sveltejs/kit').Config}
@@ -15,9 +16,12 @@ export default {
     },
     vite: {
       plugins: [
-        fetchGraphQLSchema({
-          url: 'https://graphiql-test.netlify.app/.netlify/functions/schema-demo',
-        }),
+        // Skill this rollup plugin if we are in the context of magidoc generate command
+        magidoc.MAGIDOC_GENERATE.vite.get(process.env)
+          ? fetchGraphQLSchema({
+              url: 'https://graphiql-test.netlify.app/.netlify/functions/schema-demo',
+            })
+          : null,
       ],
       ssr: {
         // TODO - this is temporary. There seems to be an issue with SSR
