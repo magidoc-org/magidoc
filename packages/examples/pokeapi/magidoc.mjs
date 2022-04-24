78bce82e6e0b496c0879ae089370b88a73cdcf81
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-04-24 14:54:14 -0400

    add poke-api example

---
+++ packages/examples/pokeapi/magidoc.mjs
@@ -0,0 +1,12 @@
+export default {
+  introspection: {
+    url: 'https://beta.pokeapi.co/graphql/v1beta',
+  },
+  website: {
+    template: 'carbon-multi-page',
+    options: {
+      appTitle: 'PokéAPI',
+      appLogo: 'https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi.svg?sanitize=true',
+    },
+  },
+}
