6a7f6e58387744f84752d4c634658d4694740e24
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-05-12 15:58:45 -0400

    add package manager chooser argument (#56)
    * add package manager chooser argument
    
    * fix format

--- packages/cli/src/commands/generate/config.ts
+++ packages/cli/src/commands/generate/config.ts
@@ -1,5 +1,7 @@
+import type { PackageManagerType } from '../../node/packageManager'
 import type { MagidocConfiguration } from './config/types'
 
 export type GenerationConfig = MagidocConfiguration & {
+  packageManager?: PackageManagerType
   clean: boolean
 }
--- packages/cli/src/commands/generate/tasks/selectPackageManager.ts
+++ packages/cli/src/commands/generate/tasks/selectPackageManager.ts
@@ -1,11 +1,22 @@
 import { newTask, GenerateTask } from '../task'
-import { selectPackageManager } from '../../../node/packageManager'
+import {
+  getPackageManager,
+  selectPackageManager,
+} from '../../../node/packageManager'
+import type { GenerationConfig } from '../config'
 
-export function selectPackageManagerTask(): GenerateTask {
+export function selectPackageManagerTask(
+  config: GenerationConfig,
+): GenerateTask {
   return newTask({
     title: 'Select Package Manager',
     executor: async (ctx, task) => {
-      ctx.packageManager = await selectPackageManager()
+      if (config.packageManager) {
+        ctx.packageManager = getPackageManager(config.packageManager)
+      } else {
+        ctx.packageManager = await selectPackageManager()
+      }
+
       task.output = `Selected ${ctx.packageManager.type}`
     },
   })
