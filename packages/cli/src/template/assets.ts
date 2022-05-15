bc530d0576f0f017d3bf9348557777139aefd4f1
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-05-15 19:12:41 -0400

    add assets file

--- packages/cli/src/template/assets.ts
+++ packages/cli/src/template/assets.ts
@@ -1,9 +1,21 @@
-import { copy } from 'fs-extra'
+import { copy, pathExists, stat } from 'fs-extra'
 
 export async function copyStaticAssets(from: string, to: string) {
-  if()
+  if (!(await pathExists(from))) {
+    throw new Error(`Source path '${from}' does not exist.`)
+  }
+
+  if (!(await isDirectory(from))) {
+    throw new Error(`Source path '${from}' is not a directory.`)
+  }
+
   await copy(from, to, {
     overwrite: true,
     recursive: true,
   })
+}
+
+async function isDirectory(path: string): Promise<boolean> {
+  const fstat = await stat(path)
+  return fstat.isDirectory()
 }
