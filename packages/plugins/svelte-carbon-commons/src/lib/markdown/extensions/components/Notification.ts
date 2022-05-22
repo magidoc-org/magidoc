d5e2648f91207b4571600aa2fe8fa67778169b9b
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-05-22 13:27:24 -0400

    add tags

---
+++ packages/plugins/svelte-carbon-commons/src/lib/markdown/extensions/components/Notification.ts
@@ -0,0 +1,32 @@
+import type { marked } from 'marked'
+import type { ContainerOptions } from '../container'
+
+const notificationStyles = ['error', 'success', 'info', 'warning'] as const
+export type NotificationStyle = typeof notificationStyles[number]
+
+export type NotificationToken = marked.Tokens.Generic & {
+  type: 'notification'
+  style: NotificationStyle
+  raw: string
+  tokens: marked.Token[]
+}
+
+export function parseNotification(
+  raw: string,
+  options: ContainerOptions,
+): NotificationToken {
+  const typeOption =
+    options['type']?.toString()?.toLocaleLowerCase()?.trim() ?? 'info'
+  const style: NotificationStyle = notificationStyles.some(
+    (it) => it === typeOption,
+  )
+    ? (typeOption as NotificationStyle)
+    : 'info'
+
+  return {
+    type: 'notification',
+    raw: raw,
+    style: style,
+    tokens: [],
+  }
+}
