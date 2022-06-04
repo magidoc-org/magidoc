eca5ebe4e11aca7fdc7320f739c17cb98fc8edbb
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-06-04 15:10:54 -0400

    cleanup last MR

--- packages/plugins/starter-variables/src/variables/readEnv.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-
-export function readEnvFile(path: string): Record<string, string> {
-  if (!fs.existsSync(path)) return {}
-  return JSON.parse(fs.readFileSync(path, 'utf8').toString()) as Record<
-    string,
-    string
-  >
-}
