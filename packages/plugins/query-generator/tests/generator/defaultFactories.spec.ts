ee2453d5eb256dc7c321c8b90f0348355588618b
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-06-12 10:02:43 -0400

    add hasura default types

--- packages/plugins/query-generator/tests/generator/defaultFactories.spec.ts
+++ packages/plugins/query-generator/tests/generator/defaultFactories.spec.ts
@@ -1,226 +1,265 @@
 import { DEFAULT_FACTORIES } from '../../src/generator/defaultFactories'
 
+export type TestSet<T> = {
+  type: string
+  names: string[]
+  expected: T
+}
+
+const ALL_SETS: ReadonlyArray<TestSet<unknown>> = [
+  {
+    type: 'String',
+    names: ['Email', 'Emails', 'mail', 'mails'],
+    expected: 'test-email@yourcompany.com',
+  },
+  {
+    type: 'String',
+    names: ['fullname', 'Fullnames'],
+    expected: 'John Doe',
+  },
+  {
+    type: 'String',
+    names: ['firstname', 'FirstNames'],
+    expected: 'John',
+  },
+  {
+    type: 'String',
+    names: ['LastName', 'lastNames'],
+    expected: 'Doe',
+  },
+  {
+    type: 'String',
+    names: ['job', 'Job', 'JobTitle', 'JobTitles'],
+    expected: 'Engineer',
+  },
+  {
+    type: 'String',
+    names: ['jobArea', 'departments', 'department'],
+    expected: 'Engineering',
+  },
+  {
+    type: 'String',
+    names: ['Phone', 'Phones'],
+    expected: '1+ 418-323-4236',
+  },
+  {
+    type: 'String',
+    names: ['country', 'Countries'],
+    expected: 'Canada',
+  },
+  {
+    type: 'String',
+    names: ['address', 'Addresses'],
+    expected: '2832 Sesame Street',
+  },
+  {
+    type: 'String',
+    names: ['State', 'states', 'province', 'provinces'],
+    expected: 'Quebec',
+  },
+  {
+    type: 'String',
+    names: ['City', 'cities', 'town', 'towns'],
+    expected: 'Montreal',
+  },
+  {
+    type: 'String',
+    names: ['zipCode', 'zipCodes'],
+    expected: 'G1G 43F',
+  },
+  {
+    type: 'String',
+    names: ['company', 'Companies'],
+    expected: 'Acme Inc',
+  },
+  {
+    type: 'String',
+    names: [
+      'datetime',
+      'datetimes',
+      'instant',
+      'timestamp',
+      'timestampz',
+      'timestamps',
+    ],
+    expected: '2022-03-06T08:23:45.000Z',
+  },
+  {
+    type: 'String',
+    names: ['date', 'dates'],
+    expected: '2022-03-06',
+  },
+  {
+    type: 'String',
+    names: ['price', 'prices'],
+    expected: '$1,234.56',
+  },
+  {
+    type: 'String',
+    names: ['color', 'colors', 'paint', 'paints'],
+    expected: '#e10098',
+  },
+  {
+    type: 'String',
+    names: ['product', 'products'],
+    expected: 'Soup',
+  },
+  {
+    type: 'String',
+    names: ['material', 'materials'],
+    expected: 'Wood',
+  },
+  {
+    type: 'String',
+    names: ['id', 'ids', 'identifier', 'identifiers', 'uuid', 'uuids'],
+    expected: '08a16b83-9094-4e89-8c05-2ccadd5c1c7e',
+  },
+  {
+    type: 'String',
+    names: ['size', 'sizes'],
+    expected: 'Large',
+  },
+  {
+    type: 'String',
+    names: ['name', 'names'],
+    expected: 'A name',
+  },
+  {
+    type: 'String',
+    names: ['description', 'descriptions'],
+    expected: 'A description',
+  },
+  {
+    type: 'String',
+    names: ['url', 'urls'],
+    expected: 'https://website.com',
+  },
+  {
+    type: 'String',
+    names: ['everythingelse', 'somestuff'],
+    expected: 'lorem ipsum',
+  },
+  {
+    type: '_text',
+    names: ['everythingelse', 'somestuff'],
+    expected: 'lorem ipsum',
+  },
+  {
+    type: 'Int',
+    names: ['salary', 'salaries'],
+    expected: 70_000,
+  },
+  {
+    type: 'Int',
+    names: ['age', 'ages'],
+    expected: 42,
+  },
+  {
+    type: 'Int',
+    names: ['anythingelse', 'asd'],
+    expected: 42,
+  },
+  {
+    type: 'Float',
+    names: ['anythingelse', 'asd'],
+    expected: 30.7,
+  },
+  {
+    type: 'Boolean',
+    names: ['anythingelse', 'asd'],
+    expected: true,
+  },
+  {
+    type: 'ID',
+    names: ['anythingelse', 'asd'],
+    expected: '08a16b83-9094-4e89-8c05-2ccadd5c1c7e',
+  },
+  {
+    type: 'Id',
+    names: ['anythingelse', 'asd'],
+    expected: '08a16b83-9094-4e89-8c05-2ccadd5c1c7e',
+  },
+  {
+    type: 'Date',
+    names: ['anythingelse', 'asd'],
+    expected: '2022-03-06',
+  },
+  {
+    type: 'Timestamp',
+    names: ['anythingelse', 'asd'],
+    expected: '2022-03-06T08:23:45.000Z',
+  },
+  {
+    type: 'Timestamptz',
+    names: ['anythingelse', 'asd'],
+    expected: '2022-03-06T08:23:45.000Z',
+  },
+  {
+    type: 'DateTime',
+    names: ['anythingelse', 'asd'],
+    expected: '2022-03-06T08:23:45.000Z',
+  },
+  {
+    type: 'Instant',
+    names: ['anythingelse', 'asd'],
+    expected: '2022-03-06T08:23:45.000Z',
+  },
+  {
+    type: 'BigInteger',
+    names: ['anythingelse', 'asd'],
+    expected: 42,
+  },
+  {
+    type: 'BigNumber',
+    names: ['anythingelse', 'asd'],
+    expected: 42,
+  },
+  {
+    type: 'Long',
+    names: ['anythingelse', 'asd'],
+    expected: 42,
+  },
+  {
+    type: 'URL',
+    names: ['anythingelse', 'asd'],
+    expected: 'https://website.com',
+  },
+  {
+    type: 'Url',
+    names: ['anythingelse', 'asd'],
+    expected: 'https://website.com',
+  },
+]
+
 describe('generating default values', () => {
   beforeAll(() => {
-    expect(Object.keys(DEFAULT_FACTORIES)).toHaveLength(12)
+    // If this fails, make sure you add a test for the new types
+    expect(Object.keys(DEFAULT_FACTORIES)).toHaveLength(45)
   })
 
-  describe('input is a string', () => {
-    test.each(['Email', 'Emails', 'mail', 'mails'])(
-      'should generate an email for input type %s',
-      (name) =>
-        expect(runFactory('String', name)).toEqual(
-          'test-email@yourcompany.com',
-        ),
-    )
+  ALL_SETS.forEach((set) => {
+    describe(`input is '${set.type}'`, () => {
+      test.each(set.names)(
+        `should generate '${set.type}' for input name %s`,
+        (input) => {
+          expect(runFactory(set.type, input)).toEqual(set.expected)
+        },
+      )
 
-    test.each(['fullname', 'Fullnames'])(
-      'should generate a full name for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('John Doe'),
-    )
+      test.each(set.names.map((name) => name.toLocaleLowerCase()))(
+        `should generate '${set.type}' for input name %s`,
+        (input) => {
+          expect(runFactory(set.type, input)).toEqual(set.expected)
+        },
+      )
 
-    test.each(['firstName', 'firstNames'])(
-      'should generate a first name for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('John'),
-    )
-
-    test.each(['LastName', 'lastNames'])(
-      'should generate a last name for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Doe'),
-    )
-
-    test.each(['job', 'Job', 'JobTitle', 'JobTitles'])(
-      'should generate a job title for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Engineer'),
-    )
-
-    test.each(['jobArea'])(
-      'should generate a job area for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Engineering'),
-    )
-
-    test.each(['Phone', 'Phones'])(
-      'should generate a phone for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('1+ 418-323-4236'),
-    )
-
-    test.each(['Country', 'Countries'])(
-      'should generate a country for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Canada'),
-    )
-
-    test.each(['address', 'addresses'])(
-      'should generate a country for input type %s',
-      (name) =>
-        expect(runFactory('String', name)).toEqual('2832 Sesame Street'),
-    )
-
-    test.each(['state', 'states'])(
-      'should generate a state for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Quebec'),
-    )
-
-    test.each(['city', 'cities'])(
-      'should generate a city for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Montreal'),
-    )
-
-    test.each(['zipCode', 'ZipCodes'])(
-      'should generate a zip code for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('G1G 43F'),
-    )
-
-    test.each(['company', 'Companies'])(
-      'should generate a company for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Acme'),
-    )
-
-    test.each(['department', 'Departments'])(
-      'should generate a department for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Engineering'),
-    )
-
-    test.each(['date', 'dates', 'datetime', 'datetimes', 'instant'])(
-      'should generate a date for input type %s',
-      (name) =>
-        expect(runFactory('String', name)).toEqual('2022-03-06T08:23:45.000Z'),
-    )
-
-    test.each(['price', 'prices'])(
-      'should generate a price for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('500$'),
-    )
-
-    test.each(['color', 'colors'])(
-      'should generate a color for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('#e10098'),
-    )
-
-    test.each(['product', 'products'])(
-      'should generate a product for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Soup'),
-    )
-
-    test.each(['material', 'materials'])(
-      'should generate a material for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Wood'),
-    )
-
-    test.each(['id', 'ids'])(
-      'should generate an id for input type %s',
-      (name) =>
-        expect(runFactory('String', name)).toEqual(
-          '08a16b83-9094-4e89-8c05-2ccadd5c1c7e',
-        ),
-    )
-
-    test.each(['name', 'names'])(
-      'should generate a name for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Some name'),
-    )
-
-    test.each(['description', 'descriptions'])(
-      'should generate a description for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('Some description'),
-    )
-
-    test.each(['everythingelse', 'somestuff'])(
-      'should generate a generic string for input type %s',
-      (name) => expect(runFactory('String', name)).toEqual('abc'),
-    )
-  })
-
-  describe('input is an int', () => {
-    test.each(['Salary', 'Salaries'])(
-      'should generate a salary for input type %s',
-      (name) => expect(runFactory('Int', name)).toEqual(70_000),
-    )
-
-    test.each(['age', 'Ages'])(
-      'should generate a salary for input type %s',
-      (name) => expect(runFactory('Int', name)).toEqual(42),
-    )
-
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic number for input type %s',
-      (name) => expect(runFactory('Int', name)).toEqual(20),
-    )
-  })
-
-  describe('input is a float', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic float for input type %s',
-      (name) => expect(runFactory('Float', name)).toEqual(30.7),
-    )
-  })
-
-  describe('input is a boolean', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic boolean for input type %s',
-      (name) => expect(runFactory('Boolean', name)).toEqual(true),
-    )
-  })
-
-  describe('input is an ID', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic ID for input type %s',
-      (name) =>
-        expect(runFactory('ID', name)).toEqual(
-          '08a16b83-9094-4e89-8c05-2ccadd5c1c7e',
-        ),
-    )
-  })
-
-  describe('input is a Date', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic date for input type %s',
-      (name) =>
-        expect(runFactory('Date', name)).toEqual('2022-03-06T08:23:45.000Z'),
-    )
-  })
-
-  describe('input is a DateTime', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic datetime for input type %s',
-      (name) =>
-        expect(runFactory('DateTime', name)).toEqual(
-          '2022-03-06T08:23:45.000Z',
-        ),
-    )
-  })
-
-  describe('input is an Instant', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic instant for input type %s',
-      (name) =>
-        expect(runFactory('Instant', name)).toEqual('2022-03-06T08:23:45.000Z'),
-    )
-  })
-
-  describe('input is a Big Integer', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic big number for input type %s',
-      (name) => expect(runFactory('BigInteger', name)).toEqual(123456789),
-    )
-  })
-
-  describe('input is a Long', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic big number for input type %s',
-      (name) => expect(runFactory('Long', name)).toEqual(123456789),
-    )
-  })
-
-  describe('input is an URL', () => {
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic big number for input type %s',
-      (name) => expect(runFactory('URL', name)).toEqual('https://website.com'),
-    )
-
-    test.each(['anythingelse', 'asd'])(
-      'should generate a generic big number for input type %s',
-      (name) => expect(runFactory('Url', name)).toEqual('https://website.com'),
-    )
+      test.each(set.names.map((name) => name[0] + name.slice(1)))(
+        `should generate '${set.type}' for input name %s`,
+        (input) => {
+          expect(runFactory(set.type, input)).toEqual(set.expected)
+        },
+      )
+    })
   })
 })
 
