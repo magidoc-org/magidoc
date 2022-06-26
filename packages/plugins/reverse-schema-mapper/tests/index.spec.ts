84db5e21d3a99e9d7d87a981aa7e149d9777bc71
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-06-26 11:41:58 -0400

    add unit tests for reverse schema mapper

--- packages/plugins/reverse-schema-mapper/tests/index.spec.ts
+++ packages/plugins/reverse-schema-mapper/tests/index.spec.ts
@@ -1,5 +1,71 @@
-describe('given a context', () => {
-  it('passes', () => {
-    expect(true).toBe(true)
+import { GraphQLNamedType, GraphQLObjectType } from 'graphql'
+import { createReverseMapping } from '../src'
+import { ReferenceKind } from '../src/reverseUsage'
+
+const schema = getTestSchema()
+const reverse = createReverseMapping(schema)
+
+describe('given schema', () => {
+  it('should create a reverse mapping for default types', () => {
+    expect(reverse.getFor(getMandatoryType('String'))?.references).toHaveLength(
+      33,
+    )
+    expect(
+      reverse.getFor(getMandatoryType('Boolean'))?.references,
+    ).toHaveLength(12)
+  })
+
+  it('should generate field references', () => {
+    const parentType = getMandatoryType('Test')
+
+    const result = reverse.getFor(getMandatoryType('Deferrable'))
+    const fieldResult = result?.references?.find(
+      (ref) => ref.kind === ReferenceKind.FIELD,
+    )
+    expect(fieldResult).toEqual({
+      kind: ReferenceKind.FIELD,
+      parent: parentType,
+      by: (parentType as GraphQLObjectType).getFields().deferrable,
+    })
+  })
+
+  it('should generate argument references', () => {
+    const targetField = getMandatoryField(
+      schema.getQueryType(),
+      'hasCustomScalarArg',
+    )
+    const result = reverse.getFor(getMandatoryType('SomeCustomScalar'))
+    const argResult = result?.references.find(
+      (ref) => ref.kind === ReferenceKind.ARGUMENT,
+    )
+
+    expect(argResult).toEqual({
+      kind: ReferenceKind.ARGUMENT,
+      by: targetField.args.find((arg) => arg.name === 'nonStandardScalar'),
+      parent: targetField,
+    })
+  })
+
+  it('should generate union references', () => {
+    const first = getMandatoryType('First')
+    const result = reverse.getFor(first)
+    const unionResult = result?.references.find(
+      (ref) => ref.kind === ReferenceKind.UNION,
+    )
+
+    expect(unionResult).toEqual({
+      kind: ReferenceKind.UNION,
+      by: getMandatoryType('TestUnion'),
+    })
   })
 })
+
+function getMandatoryType(name: string): GraphQLNamedType {
+  const result = schema.getType(name)
+  if (!result) {
+    fail(
+      `expected type '${name}' to exist but did not... did you modify the test schema?`,
+    )
+  }
+  return result
+}
