c0ad0e4d8b6e98c4a25e692a7d5b4b7e39a31731
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-08-22 21:31:33 -0400

    remove unused functon

--- packages/plugins/fuse-graphql/src/indexer/graphql/queries.ts
+++ packages/plugins/fuse-graphql/src/indexer/graphql/queries.ts
@@ -1,20 +1,7 @@
-import type { GraphQLField, GraphQLObjectType } from 'graphql'
-import type { QuerySearchResult, SearchResult } from '../result'
+import type { GraphQLField } from 'graphql'
+import type { QuerySearchResult } from '../result'
 import type { MarkdownOptions } from '@magidoc/fuse-markdown'
-import type Fuse from 'fuse.js'
 import { getDescription } from './description'
-
-export function indexAllFieldsOf(
-  type: QuerySearchResult['type'],
-  target: GraphQLObjectType | undefined | null,
-  fuse: Fuse<SearchResult>,
-  options: MarkdownOptions,
-) {
-  if (!target) return
-  Object.values(target.getFields()).forEach((field) => {
-    fuse.add(asQueryResult(type, field, options))
-  })
-}
 
 export function asQueryResult(
   type: QuerySearchResult['type'],
