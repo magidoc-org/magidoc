30452288779aefdeaaa164e2f5c65eea25109cf6
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-08-23 20:52:39 -0400

    add final search engine tweaks

--- packages/starters/carbon-multi-page/src/lib/layout/header/search/AppSearchResult.svelte
+++ packages/starters/carbon-multi-page/src/lib/layout/header/search/AppSearchResult.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import type { MagidocSearchResult } from '$lib/search'
+  import AppGraphQlSearchResult from './AppGraphQLSearchResult.svelte'
   import AppMarkdownSearchResult from './AppMarkdownSearchResult.svelte'
 
   export let item: MagidocSearchResult
 </script>
 
-{#if item.result.data.type === 'markdown'}
+{#if item.type === 'markdown'}
   <AppMarkdownSearchResult
     result={item.result}
-    indexes={item.indexes}
+    indexes={item.matches[0].indices}
     on:click
   />
+{:else if item.type === 'graphql'}
+  <AppGraphQlSearchResult {item} on:click />
 {/if}
