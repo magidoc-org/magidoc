2b6c0d0c7c48c7261313c6c6ab439ee08055abb8
Sunny Pelletier <sunnypelletier01@gmail.com>
2024-05-03 22:05:00 -0400

    Feature/support image base path (#357)
    * add base path support for images
    
    * use image path

--- packages/plugins/svelte-marked/src/lib/markdown/components/MarkdownImage.svelte
+++ packages/plugins/svelte-marked/src/lib/markdown/components/MarkdownImage.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import type { Tokens } from 'marked'
   import type { MarkdownOptions, Renderers } from '../markedConfiguration'
+  import { isRelative, joinUrlPaths } from '$lib/utils/url'
 
   export let token: Tokens.Image
-  export const options: MarkdownOptions = undefined
+  export let options: MarkdownOptions
   export const renderers: Renderers = undefined
 </script>
 
 <img
-  src={token.href}
+  src={isRelative(token.href)
+    ? joinUrlPaths(options.baseUrl, token.href)
+    : token.href}
   title={token.title}
   alt={token.text}
   class="markdown-image"
