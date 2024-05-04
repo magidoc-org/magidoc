ed41739c3a934a2656d724d7f47dde0ba7f16aa7
Sunny Pelletier <sunny.pelletier@qohash.com>
2024-05-04 11:36:15 +0900

    unifiy logo site logo path with base URL

--- packages/starters/carbon-multi-page/src/lib/components/common/AppIcon.svelte
+++ packages/starters/carbon-multi-page/src/lib/components/common/AppIcon.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
   import { DEFAULT_LOGO } from '$lib/logo'
-  import { getOrDefault } from '$lib/variables'
+  import { getOrDefault, getSiteRoot } from '$lib/variables'
 
   import { templates } from '@magidoc/plugin-starter-variables'
+  import {
+    isRelative,
+    joinUrlPaths,
+  } from '../../../../../../plugins/svelte-marked/build/utils/url'
 
-  const logoPath = getOrDefault(templates.APP_LOGO, DEFAULT_LOGO)
+  let logoPath = getOrDefault(templates.APP_LOGO, DEFAULT_LOGO)
+  logoPath = isRelative(logoPath)
+    ? joinUrlPaths(getSiteRoot(), logoPath)
+    : logoPath
 </script>
 
 <img src={logoPath} alt="Magidoc" {...$$props} />
